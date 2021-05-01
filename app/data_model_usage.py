a0e6974831b2166440746e48398319dc2bf74cef
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-04-30 19:11:44 -0700

    Change best match algorithm and regenerate model files for scikit-learn=0.24.2

--- app/data_model_usage.py
+++ app/data_model_usage.py
@@ -21,7 +21,7 @@ def display_song_entries(x):
 
 fs = FindSongs()
 
-test_vecs = ["Never gonna give you up astley", "be happy  mcferrin", "Fast Chapman", "Uptown Funk  Mars ", "I'm yours Mraz", "Walk like an egyptian bangles", "Manic Monday", "Last Christmas Wham"]
+test_vecs = ["Never gonna give you up astley", "be happy  mcferrin", "Fast Chapman", "Uptown Funk  Mars ", "I'm yours Mraz", "Walk like an egyptian bangles", "Manic Monday", "Last Christmas Wham", "Thriller Michael Jackson", "Heart-Shaped Box Nirvana","Cornet Chop Suey Louis Armstrong", "Born in the USA Bruce Springsteen", "Living on a prayer Bon Jovi", "Okay computer Radiohead", "Smooth criminal Michael Jackson"]
 
 for t in test_vecs:
 
