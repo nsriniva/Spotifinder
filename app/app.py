cf66a53519d204c19654560633d95d3ffd7015fc
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-07-02 20:03:45 -0700

    Cleaned up MVP UI

--- app/app.py
+++ app/app.py
@@ -4,10 +4,12 @@ import dash_html_components as html
 import dash_table as dt
 import plotly.express as px
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 from .data_model.find_songs import FindSongs
 
 REC_COLS = ['artist','song']
 FEATURES = ['name', 'artists']
+SPOTIFINDER = 'Spotifinder'
 
 get_song_info = lambda x:  ' '.join(x[FEATURES].to_list())
 
@@ -17,25 +19,56 @@ findSongs = FindSongs()
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
+app.title = SPOTIFINDER
+
 app.layout = html.Div([
-    html.Label("Spotifinder: Recommending Songs", style={'fontSize':40, 'textAlign':'left'}),
-    html.Label("Song Name and/or Artist(s)", style={'fontSize':30, 'textAlign':'left'}),
-    dcc.Input(
-        id='Hint',
-        type = 'text',
-        placeholder = 'Song Name and/or Artist(s)',
-        debounce=True
-    ),
-    html.Label("Songs:", style={'fontSize':30, 'textAlign':'left'}),
-    dcc.Dropdown(id='Songs',
-                 multi=False),
-    html.Label("Recommendations:", style={'fontSize':30, 'textAlign':'left'}),
-    dt.DataTable(
-        id='rec-table',
-        columns=[{"name": i.upper(), "id": i} for i in FEATURES],
-        style_cell={'textAlign': 'center'},
-        style_table={'minWidth': '360px','width': '360px','maxWidth': '360px'}
-    )
+    html.H1(children="Spotifinder: Recommending Songs", style={'textAlign':'center'}),
+    html.Div([
+        html.H2(children="Specifying Song Choice", style={'textAlign':'center'}),        
+        html.Div([
+            html.Label("Song Name and/or Artist(s):", style={'fontSize':20, 'textAlign':'left'})
+        ],
+                 style={
+                    'width':'10%',
+                     'text-align':'left',
+                     'display':'inline-block'
+                 }
+        ),
+        html.Div([
+            dcc.Input(
+                id='Hint',
+                type = 'text',
+                placeholder = 'Song Name and/or Artist(s)',
+                debounce=True
+            )
+        ],
+                 style={
+                    'width':'10%',
+                     'text-align':'left',
+                     'display':'inline-block'
+                 }
+                 
+        ),
+        html.Div([
+            dcc.Dropdown(id='Songs',
+                         multi=False)
+        ],
+                 style={
+                     'width':'70%',
+                     'vertical-align':'middle',
+                     'display':'inline-block'
+                 }
+        ),
+    ]),
+   html.Div([
+        html.H2("Recommendations", style={'textAlign':'center'}),
+        dt.DataTable(
+            id='rec-table',
+            columns=[{"name": i.upper(), "id": i} for i in FEATURES],
+            style_cell={'textAlign': 'center'},
+            style_table={'minWidth': '360px','width': '360px','maxWidth': '360px', 'marginLeft':'auto', 'marginRight':'auto'}
+        )
+    ])
 ])
 
 @app.callback(
@@ -45,7 +78,7 @@ app.layout = html.Div([
 )
 def set_options(hint):
     if hint is None:
-        hint = 'Fast Chapman'
+        raise PreventUpdate
     df = findSongs.find_song_entries(hint)
     best_idx = findSongs.get_best_choice(hint, df)
     dicosongs = [{'label': get_song_info(row), 'value': idx} for idx,row in df.iterrows()]
@@ -56,6 +89,8 @@ def set_options(hint):
     [Input('Songs', 'value')],
 )
 def predict(song):
+    if song is None:
+        raise PreventUpdate
     selected_song = findSongs.get_df_entry(song)
 
     result = findSongs.get_recommendations(selected_song)
