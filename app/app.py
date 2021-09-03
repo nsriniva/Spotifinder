e34f6a6bc30e7009e53e6bac51934776848d3114
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-09-02 20:28:46 -0700

    Integrated changes from dash_app branch

--- app/app.py
+++ app/app.py
@@ -5,8 +5,27 @@ import dash_table as dt
 import plotly.express as px
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
-from .data_model.find_songs import FindSongData, FindSongEntries, FindSongRecommendations, getBestChoice
 
+from .data_model.find_songs import FindSongData, getBestChoice
+
+from requests import post
+from os import getenv
+
+if (FIND_SONGS_URL := getenv('FIND_SONGS_URL')) is None:
+    FIND_SONGS_URL = 'http://ec2-18-117-181-89.us-east-2.compute.amazonaws.com'
+FIND_MATCHING_SONGS = FIND_SONGS_URL+'/matching_songs'
+GET_RECOMMENDED_SONGS = FIND_SONGS_URL+'/recommended_songs'
+
+extract_id_list = lambda x: [int(y) for y in x[1:-2].split(',')]
+
+def find_matching_songs(hint):
+    ret = post(FIND_MATCHING_SONGS, params={'hint':hint})
+    return extract_id_list(ret.text)
+
+
+def get_recommended_songs(selected_song):
+    ret = post(GET_RECOMMENDED_SONGS,data=selected_song.to_json())
+    return extract_id_list(ret.text)
 
 REC_COLS = ['artist','song']
 FEATURES = ['name', 'artists']
@@ -16,8 +35,6 @@ get_song_info = lambda x:  ' '.join(x[FEATURES].to_list())
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-findSongEntries = FindSongEntries()
-findSongRecommendations = FindSongRecommendations()
 findSongData = FindSongData()
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -30,39 +47,41 @@ app.layout = html.Div([
     html.Div([
         html.H2(children="Specifying Song Choice", style={'textAlign':'center'}),        
         html.Div([
-            html.Label("Song Name and/or Artist(s):", style={'fontSize':20, 'textAlign':'left'})
-        ],
-                 style={
-                    'width':'10%',
-                     'text-align':'left',
-                     'display':'inline-block'
-                 }
-        ),
-        html.Div([
-            dcc.Input(
-                id='Hint',
-                type = 'text',
-                placeholder = 'Song Name and/or Artist(s)',
-                debounce=True
-            )
-        ],
-                 style={
-                    'width':'10%',
-                     'text-align':'left',
-                     'display':'inline-block'
-                 }
-                 
-        ),
-        html.Div([
-            dcc.Dropdown(id='Songs',
-                         multi=False)
-        ],
-                 style={
-                     'width':'70%',
-                     'vertical-align':'middle',
-                     'display':'inline-block'
-                 }
-        ),
+            html.Label("Song Name and/or Artist(s):",  style={
+                'width':'20%',
+                'text-align':'left',
+                'display':'inline-block'
+            }
+            ),
+        dcc.Input(
+            id='Hint',
+            type = 'text',
+            placeholder = 'Song Name and/or Artist(s)',
+            debounce=True,
+            style={
+                'width':'30%',
+                'text-align':'left',
+                'display':'inline-block'
+            }
+        )           
+        ]),
+    ]),
+    html.Div([
+            html.Label("Chosen Song:",  style={
+                'width':'20%',
+                'text-align':'left',
+                'display':'inline-block'
+            }
+            ),
+        dcc.Dropdown(id='Songs',
+                     multi=False,
+                     style={
+                         'width':'70%',
+                         'vertical-align':'middle',
+                         'display':'inline-block'
+                     }
+        )
+        ,
     ]),
    html.Div([
         html.H2("Recommendations", style={'textAlign':'center'}),
@@ -83,7 +102,7 @@ app.layout = html.Div([
 def set_options(hint):
     if hint is None:
         raise PreventUpdate
-    entries = findSongEntries.find_matching_songs(hint)
+    entries = find_matching_songs(hint)
     df = findSongData.get_song_entries_data(entries, sorted=True)
     best_idx = getBestChoice(hint, df)
     dicosongs = [{'label': get_song_info(row), 'value': idx} for idx,row in df.iterrows()]
@@ -97,8 +116,7 @@ def predict(song):
     if song is None:
         raise PreventUpdate
     selected_song = findSongData.get_df_entry(song)
-    
-    entries = findSongRecommendations.get_recommended_songs_json(selected_song.to_json())
+    entries = get_recommended_songs(selected_song)
     result = findSongData.get_song_entries_data(entries)
 
     return result[FEATURES].to_dict('records')
@@ -106,3 +124,4 @@ def predict(song):
     
 if __name__ == '__main__':
     app.run_server(debug=True)
+
